ab55c6eb9201fe32b40aab2bce18722441d20a81
Keisha Hepner <kmhepner90@gmail.com>
2022-12-21 01:01:21 -0800

    Add Scroll animation

--- src/index.js
+++ src/index.js
@@ -21,3 +21,19 @@ navLinks.forEach(link => {
     link.classList.remove("hovered-link");
   });
 });
+
+// Page Load
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
